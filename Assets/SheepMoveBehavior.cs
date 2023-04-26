0d4f1aca32d28cca017ac2f2937ee63e427dd6f4
CryptReader86 <wilson.ariza@gmail.com>
2023-04-26 14:38:58 +0200

    drawing too many sheeps with the old system

--- Assets/SheepMoveBehavior.cs
+++ Assets/SheepMoveBehavior.cs
@@ -5,8 +5,18 @@ public class SheepMoveBehavior : MonoBehaviour
     [SerializeField]
     private float _sheepSpeed;
 
+    [SerializeField]
+    private float _zLimit;
+
     void Update()
     {
         transform.Translate(_sheepSpeed * Time.deltaTime * transform.forward);
+
+        var position = transform.position;
+        if (position.z >= _zLimit)
+        {
+            position.z = -_zLimit;
+            transform.position = position;
+        }
     }
 }
