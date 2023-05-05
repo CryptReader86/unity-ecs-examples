778b7934b54a110f08454dae3f3c2390543e3a9a
CryptReader86 <wilson.ariza@gmail.com>
2023-05-05 15:00:22 +0200

    ECSManager WIP and hybrid renderer package added

--- Assets/ECSManager.cs
+++ Assets/ECSManager.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public class ECSManager : MonoBehaviour
 {
-    
+    private EntityManager _entityManager;
+
+    [SerializeField]
+    private int _numSheep;
+
+    [SerializeField]
+    private GameObject _sheepPrefab;
+
+    private void Start()
+    {
+        _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+    }
 }
