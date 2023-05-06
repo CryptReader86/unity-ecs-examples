d1c8e20afbb63e7fb234d504d6d52437e8d69ddd
CryptReader86 <wilson.ariza@gmail.com>
2023-05-06 07:40:08 +0200

    instancing sheeps with ECS

--- Assets/ECSManager.cs
+++ Assets/ECSManager.cs
@@ -16,5 +16,17 @@ public class ECSManager : MonoBehaviour
     private void Start()
     {
         _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(_sheepPrefab, settings);
+
+        for (var i = 0; i < _numSheep; i++)
+        {
+            var sheep = _entityManager.Instantiate(prefab);
+            var sheepPosition = new float3(UnityEngine.Random.Range(-50.0f, 50.0f), 0, UnityEngine.Random.Range(-50.0f, 50.0f));
+
+            _entityManager.SetComponentData(sheep, new Translation { Value = sheepPosition });
+            _entityManager.SetComponentData(sheep, new Rotation { Value = new quaternion(0, 0, 0, 0) });
+        }
     }
 }
