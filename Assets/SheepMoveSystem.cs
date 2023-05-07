fb0bf4c2ef327915371e11d50949121b02f47caa
CryptReader86 <wilson.ariza@gmail.com>
2023-05-07 08:02:48 +0200

    first example of ECS working 15000 sheep at 120 fps

---
+++ Assets/SheepMoveSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class SheepMoveSystem : JobComponentSystem
+{
+    private const float ZLimit = 50.0f;
+
+    private const float SheepSpeed = 0.1f;
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return Entities
+            .WithName("SheepMoveSystem")
+            .ForEach((ref Translation position, ref Rotation rotation) =>
+            {
+                position.Value += SheepSpeed * math.forward(rotation.Value);
+                if (position.Value.z >= ZLimit)
+                {
+                    position.Value.z = -ZLimit;
+                }
+            })
+            .Schedule(inputDeps);
+    }
+}
