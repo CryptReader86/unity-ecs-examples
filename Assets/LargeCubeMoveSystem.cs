a3342326abff7bc62400265e78976b0c05afcdf5
CryptReader86 <wilson.ariza@gmail.com>
2023-05-16 09:48:09 +0200

    adding IComponentData challenge

---
+++ Assets/LargeCubeMoveSystem.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class LargeCubeMoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities
+            .WithName("MoveSystem")
+            .ForEach((ref Translation position, ref LargeCubeData largeCubeData) =>
+            {
+                position.Value -= 0.01f * math.up();
+            })
+            .Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
