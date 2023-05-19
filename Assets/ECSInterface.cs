da1ff5d661037d9f63aaab26f49ad9200b0a0176
CryptReader86 <wilson.ariza@gmail.com>
2023-05-19 10:06:43 +0200

    Adding an Entity from Monobehaviour at Runtime

--- Assets/ECSInterface.cs
+++ Assets/ECSInterface.cs
@@ -1,18 +1,47 @@
 ﻿using UnityEngine;
 using TMPro;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public class ECSInterface : MonoBehaviour
 {
+    private World _world;
     private EntityManager _entityManager;
 
     [SerializeField]
     private TMP_Text _label;
 
+    [SerializeField]
+    private GameObject _sheepPrefab;
+
+    [SerializeField]
+    private GameObject _palmTreePrefab;
+
     private void Start()
     {
-        var world = World.DefaultGameObjectInjectionWorld;
-        _entityManager = world.GetExistingSystem<MoveSystem>().EntityManager;
+        _world = World.DefaultGameObjectInjectionWorld;
+        _entityManager = _world.GetExistingSystem<MoveSystem>().EntityManager;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_sheepPrefab, new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), 0, UnityEngine.Random.Range(-10.0f, 10.0f)), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            var settings = GameObjectConversionSettings.FromWorld(_world, null);
+            var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(_palmTreePrefab, settings);
+
+            var palmTree = _entityManager.Instantiate(prefab);
+            var palmTreePosition = new float3(UnityEngine.Random.Range(-10.0f, 10.0f), 0, UnityEngine.Random.Range(-10.0f, 10.0f));
+
+            _entityManager.SetComponentData(palmTree, new Translation { Value = palmTreePosition });
+            _entityManager.SetComponentData(palmTree, new Rotation { Value = new quaternion(0, 0, 0, 0) });
+        }
     }
 
     public void OnButtonPressed()
