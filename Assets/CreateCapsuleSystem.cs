585e35f592afc9da34bb2357501048ff8b1e397a
CryptReader86 <wilson.ariza@gmail.com>
2023-05-23 10:04:43 +0200

    organizing files and building an entity from scratch

---
+++ Assets/CreateCapsuleSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Jobs;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+
+public class CreateCapsuleSystem : JobComponentSystem
+{
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        var entity = EntityManager.CreateEntity(
+                ComponentType.ReadWrite<Translation>(),
+                ComponentType.ReadWrite<Rotation>(),
+                ComponentType.ReadOnly<RenderMesh>()
+            );
+
+        EntityManager.SetComponentData(entity, new Translation { Value = float3.zero });
+        EntityManager.SetComponentData(entity, new Rotation { Value = quaternion.identity });
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return inputDeps;
+    }
+}
---
+++ Assets/ResourceHolder.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class ResourceHolder : MonoBehaviour
+{
+    [SerializeField] private Mesh _theMesh;
+    [SerializeField] private Material _theMaterial;
+}
