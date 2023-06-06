82ae2acc0bf34d5840b7b99daf3fdf255c77af49
CryptReader86 <wilson.ariza@gmail.com>
2023-06-06 15:20:26 +0200

    pivoting challenge

--- Assets/ECSManager.cs
+++ Assets/ECSManager.cs
@@ -5,26 +5,30 @@ using Unity.Transforms;
 
 public class ECSManager : MonoBehaviour
 {
-    [SerializeField]
-    public GameObject _planetPrefab;
-
-    private const int NumPlanets = 500;
+    EntityManager manager;
+    public GameObject asteroidPrefab;
+    const int numAsteroids = 5000;
 
     void Start()
     {
-        var manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
         var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
-        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(_planetPrefab, settings);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(asteroidPrefab, settings);
 
-        for (int i = 0; i < NumPlanets; i++)
+        for (int i = 0; i < numAsteroids; i++)
         {
             var instance = manager.Instantiate(prefab);
-
-            float x = UnityEngine.Random.Range(-100, 100);
-            float z = UnityEngine.Random.Range(-100, 100);
-            var position = transform.TransformPoint(new float3(x, 0, z));
-
+            float x = Mathf.Sin(i) * UnityEngine.Random.Range(25, 50);
+            float z = Mathf.Cos(i) * UnityEngine.Random.Range(25, 50);
+            var position = transform.TransformPoint(new float3(x, UnityEngine.Random.Range(-2.0f, 2.0f), z));
             manager.SetComponentData(instance, new Translation { Value = position });
+
+            var q = Quaternion.Euler(new Vector3(0, 0, 0));
+            manager.SetComponentData(instance, new Rotation { Value = new quaternion(q.x,q.y,q.z,q.w) });
+
+            var scale = UnityEngine.Random.Range(25.0f, 100.0f);
+            manager.SetComponentData(instance, new NonUniformScale { Value = new float3(scale) });
         }
+
     }
 }
---
+++ Assets/Lessons/WorkingWithEntities_4/ECSManager.cs
@@ -0,0 +1,32 @@
+﻿/*
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ECSManager : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject _planetPrefab;
+
+    private const int NumPlanets = 500;
+
+    void Start()
+    {
+        var manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(_planetPrefab, settings);
+
+        for (int i = 0; i < NumPlanets; i++)
+        {
+            var instance = manager.Instantiate(prefab);
+
+            float x = UnityEngine.Random.Range(-100, 100);
+            float z = UnityEngine.Random.Range(-100, 100);
+            var position = transform.TransformPoint(new float3(x, 0, z));
+
+            manager.SetComponentData(instance, new Translation { Value = position });
+        }
+    }
+}
+*/
