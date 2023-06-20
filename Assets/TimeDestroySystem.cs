90e43eb616a3685b5557ffedb50891081452d311
CryptReader86 <wilson.ariza@gmail.com>
2023-06-20 10:01:42 +0200

    restricting the lifetime of an entity update

--- Assets/TimeDestroySystem.cs
+++ Assets/TimeDestroySystem.cs
@@ -7,44 +7,21 @@ using Unity.Collections;
 [UpdateAfter(typeof(MoveBulletSystem))]
 public class TimeDestroySystem : JobComponentSystem
 {
-    struct CullingJob : IJobForEachWithEntity<LifetimeData>
-    {
-        public EntityCommandBuffer.Concurrent commands;
-        public float deltaTime;
-
-        public void Execute(Entity entity, int index, ref LifetimeData lifetimeData)
-        {
-            lifetimeData.lifeLeft -= deltaTime;
-            if(lifetimeData.lifeLeft <= 0)
-            {
-                commands.DestroyEntity(index, entity);
-            }
-        }
-    }
-
-    private EndSimulationEntityCommandBufferSystem _buffer;
-
-    protected override void OnCreate()
-    {
-        base.OnCreate();
-
-        _buffer = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-    }
-
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        var cullingJob = new CullingJob
-        {
-            commands = _buffer.CreateCommandBuffer().ToConcurrent(),
-            deltaTime = Time.DeltaTime
-        };
+        float deltaTime = Time.DeltaTime;
 
-        var jobHandle = cullingJob.Schedule(this, inputDeps);
-        _buffer.AddJobHandleForProducer(jobHandle);
-
-        jobHandle.Complete();
+        Entities.WithoutBurst().WithStructuralChanges()
+            .ForEach((Entity entity, ref LifetimeData lifetimeData) =>
+            {
+                lifetimeData.lifeLeft -= deltaTime;
+                if (lifetimeData.lifeLeft <= 0)
+                {
+                    EntityManager.DestroyEntity(entity);
+                }
+            })
+            .Run();
 
         return inputDeps;
     }
-
 }
