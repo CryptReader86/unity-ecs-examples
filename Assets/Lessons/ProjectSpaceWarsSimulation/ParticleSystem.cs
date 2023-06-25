36a927e488365aed0517bf3965271cc3148ec883
CryptReader86 <wilson.ariza@gmail.com>
2023-06-25 09:08:54 +0200

    a system for scaling properties

---
+++ Assets/Lessons/ProjectSpaceWarsSimulation/ParticleSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Collections;
+
+public class ParticleSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        var jobHandle = Entities
+               .WithName("ParticleSystem")
+               .ForEach((ref NonUniformScale scale, ref ParticleData particleData) =>
+               {
+                   particleData.timeAlive += deltaTime;
+                   scale.Value = particleData.timeAlive * 2.0f;
+               })
+               .Schedule(inputDeps);
+
+        jobHandle.Complete();
+
+        return jobHandle;
+    }
+}
