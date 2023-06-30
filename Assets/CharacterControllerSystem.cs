03d0e5e40c399b19114b029a243a25fac798ff75
CryptReader86 <wilson.ariza@gmail.com>
2023-06-30 10:37:21 +0200

    an ecs physics based character controller

---
+++ Assets/CharacterControllerSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Physics;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CharacterControllerSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var deltaTime = Time.DeltaTime;
+        var inputY = Input.GetAxis("Horizontal");
+        var inputZ = Input.GetAxis("Vertical");
+
+        var jobHandle = Entities
+            .WithName("CharacterControllerSystem")
+            .ForEach((ref PhysicsVelocity physicsVelocity, ref Rotation rotation, ref CharacterData characterData) =>
+            {
+                if (inputZ == 0)
+                    physicsVelocity.Linear = float3.zero;
+                else
+                    physicsVelocity.Linear = inputZ * deltaTime * characterData.speed * math.forward(rotation.Value);
+
+                rotation.Value = math.mul(math.normalize(rotation.Value), quaternion.AxisAngle(math.up(), deltaTime * inputY));
+            })
+            .Schedule(inputDeps);
+
+        jobHandle.Complete();
+
+        return inputDeps;
+    }
+}
---
+++ Assets/CharacterData.cs
@@ -0,0 +1,7 @@
+﻿using Unity.Entities;
+
+[GenerateAuthoringComponent]
+public struct CharacterData : IComponentData
+{
+    public float speed;
+}
