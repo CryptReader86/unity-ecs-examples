ada02b70b20d2b29eb323c2a0674f054a619ceea
CryptReader86 <wilson.ariza@gmail.com>
2023-07-03 08:50:27 +0200

    spawning multiple physics entities

---
+++ Assets/DuckSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class DuckSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _duckPrefab;
+    [SerializeField] private int _numDucks;
+
+    private void Start()
+    {
+        var entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(_duckPrefab, settings);
+
+        for(var i = 0; i < _numDucks; i++)
+        {
+            var instance = entityManager.Instantiate(prefab);
+            var x = UnityEngine.Random.Range(-200.0f, 200.0f);
+            var y = UnityEngine.Random.Range(50.0f, 200.0f);
+            var z = UnityEngine.Random.Range(-200.0f, 200.0f);
+            entityManager.SetComponentData(instance, new Translation { Value = new float3(x, y, z) });
+        }
+    }
+}
